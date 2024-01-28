a0baa134c9c44f3b502f01692bc4eb26935b7ccd
partpia <partanenppia@gmail.com>
2024-01-28 19:05:32 +0200

    Initial commit

---
+++ src/test/java/hh/ont/shiftbooking/ShiftbookingApplicationTests.java
@@ -0,0 +1,13 @@
+package hh.ont.shiftbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftbookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
