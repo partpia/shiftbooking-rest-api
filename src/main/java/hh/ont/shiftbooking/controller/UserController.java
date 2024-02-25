bb9d42c67b3dc86156c4a48bf2d03f32f76e2de1
partpia <partanenppia@gmail.com>
2024-02-25 13:55:37 +0200

    UserController POST-operaatiolla sekä UserDetailService lisätty

---
+++ src/main/java/hh/ont/shiftbooking/controller/UserController.java
@@ -0,0 +1,41 @@
+package hh.ont.shiftbooking.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import hh.ont.shiftbooking.dto.CreateUserDto;
+import hh.ont.shiftbooking.service.UserDetailService;
+import jakarta.validation.Valid;
+
+
+@RestController
+@RequestMapping("accounts")
+public class UserController {
+
+    @Autowired
+    private UserDetailService service;
+    
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> createNewUser(@Valid @RequestBody CreateUserDto userDTO) throws Exception {
+
+        if (userDTO.getPassword().equals(userDTO.getPasswordCheck())) {
+            boolean userCreated = service.saveNewUser(userDTO);
+            return userCreated ? new ResponseEntity<>(
+                "Tili luotu käyttäjätunnuksella " + userDTO.getUsername(),
+                HttpStatus.CREATED) :
+                new ResponseEntity<>(
+                "Tilin luonti epäonnistui",
+                HttpStatus.INTERNAL_SERVER_ERROR);
+        } else {
+            return new ResponseEntity<>(
+                "Salasanat eivät täsmää.",
+                HttpStatus.BAD_REQUEST);
+        }
+    }
+}
---
+++ src/main/java/hh/ont/shiftbooking/service/UserDetailService.java
@@ -0,0 +1,40 @@
+package hh.ont.shiftbooking.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import hh.ont.shiftbooking.dto.CreateUserDto;
+import hh.ont.shiftbooking.model.User;
+import hh.ont.shiftbooking.repository.UserRepository;
+
+@Service
+public class UserDetailService {
+
+    @Autowired
+    private UserRepository repository;
+    
+    public boolean saveNewUser(CreateUserDto uDto) throws Exception {
+
+        if (!checkUserExists(uDto.getUsername())) {
+            User user = new User();
+            user.setFirstName(uDto.getFirstName());
+            user.setLastName(uDto.getLastName());
+            user.setEmail(uDto.getEmail());
+            user.setTel(uDto.getTel());
+            user.setUsername(uDto.getUsername());
+            // TODO: hash password
+            //String pwd = uDto.getPassword();
+            user.setPassword(uDto.getPassword());
+            user.setRole(uDto.getRole());
+            repository.save(user);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean checkUserExists(String username) {
+        return repository.findByUsername(username).isPresent();
+    }
+
+}
