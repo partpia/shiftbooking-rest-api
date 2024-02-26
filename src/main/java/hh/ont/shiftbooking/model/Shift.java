f9ad534de0fd6a59b00d986bcf97f53c09386b07
partpia <partanenppia@gmail.com>
2024-02-26 20:15:15 +0200

    Shift & Workplace > JoinColumn vaihdettu PrimaryKeyJoinColumn

--- src/main/java/hh/ont/shiftbooking/model/Shift.java
+++ src/main/java/hh/ont/shiftbooking/model/Shift.java
@@ -5,7 +5,6 @@ import java.time.LocalDateTime;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import hh.ont.shiftbooking.enums.ShiftStatus;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -14,6 +13,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotEmpty;
@@ -58,8 +58,8 @@ public class Shift {
     private User employee;
 
     @NotNull
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "WORKPLACE", referencedColumnName = "WORKPLACE_ID")
+    @OneToOne
+    @PrimaryKeyJoinColumn
     private Workplace location;
 
     public Shift(LocalDateTime startDateTime, LocalDateTime endDateTime, String description,
--- src/main/java/hh/ont/shiftbooking/model/Workplace.java
+++ src/main/java/hh/ont/shiftbooking/model/Workplace.java
@@ -8,18 +8,21 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(name = "WORKPLACE")
 @Getter
 @Setter
 @NoArgsConstructor
+@ToString
 public class Workplace {
     
     @Id
@@ -39,9 +42,9 @@ public class Workplace {
     @NotEmpty
     private String tel;
 
-    @OneToOne
-    @JoinColumn(name = "ZIP", referencedColumnName = "POST_ID")
     @NotNull
+    @OneToOne
+    @PrimaryKeyJoinColumn
     private PostOffice zip;
 
     @ManyToOne
