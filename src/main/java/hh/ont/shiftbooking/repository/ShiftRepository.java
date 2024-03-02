dfaeaa03137038598aab363e2ec22d9702df50dd
partpia <partanenppia@gmail.com>
2024-03-02 20:59:24 +0200

    ShiftRepository > custom tietokantakysely (varattavissa olevien työvuorojen haku)

--- src/main/java/hh/ont/shiftbooking/repository/ShiftRepository.java
+++ src/main/java/hh/ont/shiftbooking/repository/ShiftRepository.java
@@ -1,9 +1,16 @@
 package hh.ont.shiftbooking.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import hh.ont.shiftbooking.enums.ShiftStatus;
 import hh.ont.shiftbooking.model.Shift;
 
 public interface ShiftRepository extends CrudRepository<Shift, Long> {
+
+    @Query("select s from Shift s where s.status = ?1")
+    List<Shift> findByStatus(ShiftStatus status);
     
 }
