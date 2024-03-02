0753c6a13fb5f1c04a7c4d1756371bef711c6ed1
partpia <partanenppia@gmail.com>
2024-03-02 20:57:11 +0200

    ShiftController & -Service > GET: varattavissa olevien työvuorojen haku

--- src/main/java/hh/ont/shiftbooking/controller/ShiftController.java
+++ src/main/java/hh/ont/shiftbooking/controller/ShiftController.java
@@ -1,9 +1,12 @@
 package hh.ont.shiftbooking.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,11 +25,18 @@ public class ShiftController {
     @Autowired
     ShiftService shiftService;
     
+    // uuden työvuoron lisääminen
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Object> createShift(@Valid @RequestBody Shift shift) throws Exception {
 
             Shift created = shiftService.saveShift(shift);
             ShiftResponseDto dto = new ShiftResponseDto(created);
             return new ResponseEntity<>(dto, HttpStatus.CREATED);
+    }
+
+    // varattavissa olevien työvuorojen hakeminen
+    @GetMapping()
+    public List<ShiftResponseDto> getBookableShifts() throws Exception {
+        return shiftService.getAllBookableShifts();
     }
 }
--- src/main/java/hh/ont/shiftbooking/service/ShiftService.java
+++ src/main/java/hh/ont/shiftbooking/service/ShiftService.java
@@ -1,11 +1,14 @@
 package hh.ont.shiftbooking.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
+import hh.ont.shiftbooking.dto.ShiftResponseDto;
 import hh.ont.shiftbooking.enums.ShiftStatus;
 import hh.ont.shiftbooking.exception.DatabaseException;
 import hh.ont.shiftbooking.model.Shift;
@@ -22,8 +25,8 @@ public class ShiftService {
     @Autowired
     WorkplaceRepository workplaceRepository;
 
+    // tallentaa uuden työvuoron tiedot tietokantaan
     public Shift saveShift(Shift shift) throws DatabaseException {
-
         try {
             shift.setStatus(ShiftStatus.BOOKABLE);
             // haetaan työpaikan tiedot
@@ -36,5 +39,26 @@ public class ShiftService {
         } catch (DataAccessException e) {
             throw new DatabaseException("Tietojen tallennus epäonnistui.");
         }
+    }
+
+    // hakee varattavissa olevat työvuorot tietokannasta
+    public List<ShiftResponseDto> getAllBookableShifts() throws DatabaseException {
+        try {
+            List<Shift> list = (List<Shift>) shiftRepository.findByStatus(ShiftStatus.BOOKABLE);
+            return !list.isEmpty() ? convertShiftListToDtos(list) : new ArrayList<>();
+        } catch (Exception e) {
+            throw new DatabaseException("Työvuoroja ei voida näyttää.");
+        }
+    }
+
+    // muuntaa Shift-entityt dto-muotoon
+    private List<ShiftResponseDto> convertShiftListToDtos(List<Shift> shifts) {
+        List <ShiftResponseDto> dtos = new ArrayList<>();
+
+        for (Shift shift : shifts) {
+            ShiftResponseDto dto = new ShiftResponseDto(shift);
+            dtos.add(dto);
+        }
+        return dtos;
     }
 }
