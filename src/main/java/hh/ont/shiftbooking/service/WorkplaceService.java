bb3aefc6358b3e2c8b524bfd25d085fe7637f711
partpia <partanenppia@gmail.com>
2024-09-27 21:07:39 +0300

    WorkplaceService > Autowired vaihdettu konstruktoriin, lisätty työnantajan tiedot työpaikan tallenukseen

--- src/main/java/hh/ont/shiftbooking/service/WorkplaceService.java
+++ src/main/java/hh/ont/shiftbooking/service/WorkplaceService.java
@@ -1,30 +1,39 @@
 package hh.ont.shiftbooking.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import hh.ont.shiftbooking.dto.WorkplaceResponseDto;
 import hh.ont.shiftbooking.exception.DatabaseException;
 import hh.ont.shiftbooking.model.PostOffice;
+import hh.ont.shiftbooking.model.User;
 import hh.ont.shiftbooking.model.Workplace;
+import hh.ont.shiftbooking.repository.UserRepository;
 import hh.ont.shiftbooking.repository.WorkplaceRepository;
 
 @Service
 public class WorkplaceService {
     
-    @Autowired
-    private WorkplaceRepository workRepository;
+    private final WorkplaceRepository workRepository;
+    private final PostOfficeService postService;
+    private final UserRepository userRepository;
 
-    @Autowired
-    private PostOfficeService postService;
+    public WorkplaceService(WorkplaceRepository workRepository, PostOfficeService postService,
+            UserRepository userRepository) {
+        this.workRepository = workRepository;
+        this.postService = postService;
+        this.userRepository = userRepository;
+    }
 
+    // tallentaa uuden työpaikan tiedot tietokantaan, palauttaa dto:n
     public WorkplaceResponseDto saveWorkplace(Workplace workplace) throws Exception {
 
         try {
             // haetaan postitoimipaikan tiedot
             PostOffice postOffice = postService.getPostOfficeDetails(workplace.getZip());
             workplace.setZip(postOffice);
-            // TODO: lisää contactPerson
+            // haetaan työnantajan tiedot
+            User employer = userRepository.findById(workplace.getContactPerson().getUserId()).get();
+            workplace.setContactPerson(employer);
             Workplace saved = workRepository.save(workplace);
             return new WorkplaceResponseDto(saved);
         } catch (IllegalArgumentException e) {
