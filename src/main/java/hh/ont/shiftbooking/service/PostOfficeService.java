34c6175e856f45ffa0218cfa1f1536bf68ab436d
partpia <partanenppia@gmail.com>
2024-09-28 11:16:37 +0300

    PostOfficeService > lisätty konstruktori, transactional ja Javadoc

--- src/main/java/hh/ont/shiftbooking/service/PostOfficeService.java
+++ src/main/java/hh/ont/shiftbooking/service/PostOfficeService.java
@@ -1,18 +1,30 @@
 package hh.ont.shiftbooking.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import hh.ont.shiftbooking.exception.DatabaseException;
 import hh.ont.shiftbooking.model.PostOffice;
 import hh.ont.shiftbooking.repository.PostOfficeRepository;
+import jakarta.transaction.Transactional;
 
 @Service
 public class PostOfficeService {
     
-    @Autowired
-    private PostOfficeRepository repository;
+    private final PostOfficeRepository repository;
 
+    public PostOfficeService (PostOfficeRepository repository) {
+        this.repository = repository;
+    }
+
+    /**
+     * Tarkastaa onko postitoimipaikan tiedot tietokannassa:
+     * <p>
+     * true: palautta olemassa olevan, false: tallentaa uuden postitoimipaikan tiedot
+     * @param po PostOffice-objekti
+     * @return Postitoimipaikan tiedot (PostOffice) 
+     * @throws Exception
+     */
+    @Transactional
     public PostOffice getPostOfficeDetails(PostOffice po) throws Exception {
 
         try {
