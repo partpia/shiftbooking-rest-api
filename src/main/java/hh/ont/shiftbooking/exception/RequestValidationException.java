b07dda1cc2d256cc16adfff0b19f285bb48efb6d
partpia <partanenppia@gmail.com>
2024-09-28 11:18:09 +0300

    Oma poikkeus > RequestValidationException

---
+++ src/main/java/hh/ont/shiftbooking/exception/RequestValidationException.java
@@ -0,0 +1,8 @@
+package hh.ont.shiftbooking.exception;
+
+public class RequestValidationException extends Exception {
+
+    public RequestValidationException(String errorMessage) {
+        super(errorMessage);
+    }
+}
